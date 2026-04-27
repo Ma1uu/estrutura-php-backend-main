e0f66df13573c2951ec408102278ed80ff2ad886
Maria <mariaclarakirschner@gmail.com>
2026-04-27 14:29:35 -0300

    .

--- views/categoria/salvar.php
+++ views/categoria/salvar.php
@@ -6,23 +6,21 @@
     <form method="post" name="formsalvar" id="formSalvar" class="m-3" enctype="multipart/form-data">
 
         <div class="form-group row">
-            <label for="inputText" class="col-sm-2 col-form-label">
+            <label for="txtnome" class="col-sm-2 col-form-label">
                 Nome
             </label>
             <div class="col-sm-10">
-                <input type="text" class="form-control" id="txtnome" name="txtnome" placeholder="Estado"
+                <input type="text" class="form-control" id="txtnome" name="txtnome" placeholder="Categoria"
                     value="">
             </div>
         </div>
         <div class="form-group row">
-            <label for="inputText" class="col-sm-2 col-form-label">
-                Sigla - UF
+            <label for="txtinformacoes" class="col-sm-2 col-form-label">
+                Informações
             </label>
             <div class="col-sm-10">
-                <input type="text" class="form-control" id="txtsigla" name="txtsigla" placeholder="UF"
-                    value="">
+                <textarea name="txtinformacoes" id="txtinformacoes" rows="3" placeholder="Informações aqui" class="form-control" ></textarea>
             </div>
-        </div>
 
         <div class="form-group row">
             <div class="col-sm-10">
@@ -36,3 +34,36 @@
         </div>
     </form>
 </div>
+
+<?php
+    //verificar se botão btnsalvar foi acionado
+    if(filter_input(INPUT_POST, 'btnsalvar')){
+        $nome = filter_input(INPUT_POST, 'txtnome');
+        $info = filter_input(INPUT_POST, 'txtinformacoes');
+
+    //acesso a classe em models
+        include_once '../models/categoria.php';
+        $cat = new Categoria();
+
+    //enviando os dados do form aos aributos de classe
+    $cat->setId(NULL);
+    $cat->setNome($nome);
+    $cat->setinformacoes($info);
+
+    //efetivar o insert into
+    if($cat->salvar()) {
+        ?>
+            <div class="alert alert-primary mt-3" role="alert">
+                Categoria - Cadastro efetuado com sucesso.
+            </div>
+
+        <?php
+        } else {
+            ?>
+            <div class="alert alert-danger mt-3" role="alert">
+                Categoria - Erro ao cadastrar.
+            </div>
+
+        <?php
+        }
+    }
